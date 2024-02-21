a6a951ac10f859f8dffb4d38dc0d2d849a6839a7
Vũ Tuấn Hải <94612982+tulliavu@users.noreply.github.com>
2024-02-21 14:26:30 +0700

    Updte

--- crawdata.py
+++ crawdata.py
@@ -3,29 +3,31 @@ from binance.client import Client
 import pandas as pd
 from csv import writer
 #symbol sẽ là 1 cái list gồm 100 mã cổ phiếu của binance
-SYMBOL = ['BTCUSDT','ETHUSDT','SOLUSDT','XRPUSDT'
-        #   ,'1INCHUSDT'
-        #   'AAVEBTC','AAVEBUSD','ATOMBNB','ATOMBTC','AAVEUSDT',
-        #   'ACMBTC','ACMBUSD','ACMUSDT','ADAAUD','ADABIDR',
-        #   'ATOMBUSD','ADABNB','ADABRL','ADABTC','ADABUSD',
-        #   'ADADOWNUSDT','ADAETH','ADAEUR','ADAGBP','AXSBTC',
-        #   'ADARUB','ADATRY','ADATUSD','ADAUPUSDT','ADAUSDC',
-        #   'ADAUSDT','AXSBUSD','ADXBTC','ADXETH','ATOMUSDC',
-        #   'ATOMUSDT','AUCTIONBTC','AERGOBTC','AERGOBUSD','AUCTIONBUSD',
-        #   'AUDBUSD','AUDIOBTC','AGIXBTC','AUDIOBUSD','AIONBTC',
-        #   'AUDIOUSDT','AIONETH','AIONUSDT','AKROBTC','AKROUSDT',
-        #   'ALGOBNB','ALGOBTC','ALGOBUSD','AUDUSDT','AUTOBTC',
-        #   'ALGOUSDT','ALICEBTC','ALICEBUSD','ALICEUSDT','ALPHABNB',
-        #   'ALPHABTC','ALPHABUSD','ALPHAUSDT','AUTOBUSD','AMBBTC',
-        #   'AUTOUSDT','ANKRBNB','ANKRBTC','AVABNB','AVABTC',
-        #   'AVABUSD','ANKRUSDT','ANTBNB','ANTBTC','ANTBUSD',
-        #   'ANTUSDT','AVAUSDT','APPCBTC','AVAXBNB','ARBNB',
-        #   'ARBTC','ARBUSD','AVAXBTC','ARDRBTC','AVAXBUSD',
-        #   'ARDRUSDT','ARKBTC','AVAXEUR','AVAXTRY','AVAXUSDT',
-        #   'ASRUSDT','ASTBTC','AXSBNB','ATABNB','ATABTC',
-        #   'ATABUSD','ATAUSDT','ATMBTC','ATMBUSD','ATMUSDT'
-        ]
+SYMBOL = ['BTCUSDT','ETHUSDT','SOLUSDT','XRPUSDT','1INCHUSDT',
+          'ALCXUSDT','ALGOUSDT','ALICEUSDT','ALPACAUSDT','ALPHAUSDT',
+            'AAVEUSDT','ACAUSDT','ACMUSDT','ALPINEUSDT','ACHUSDT',
+            'ADADOWNUSDT', 'ADAUPUSDT','ADAUSDT','ADXUSDT','AERGOUSDT',
+           'AEURUSDT','AGIXUSDT','AGLDUSDT','AIONUSDT','AIUSDT',
+           'AKROUSDT','ADATRY','ADATUSD','ADAUPUSDT','ADAUSDC',
+          'ADAUSDT','AXSBUSD','ADXBTC','ADXETH','ATOMUSDC',
+          'ATOMUSDT','AUCTIONBTC','AERGOBTC','AERGOBUSD','AUCTIONBUSD',
+          'AUDBUSD','AUDIOBTC','AGIXBTC','AUDIOBUSD','AIONBTC',
+          'AUDIOUSDT','AIONETH','AIONUSDT','AKROBTC','AKROUSDT',
+          'ALGOBNB','ALGOBTC','ALGOBUSD','AUDUSDT','AUTOBTC',
+          'ALGOUSDT','ALICEBTC','ALICEBUSD','ALICEUSDT','ALPHABNB',
+          'ALPHABTC','ALPHABUSD','ALPHAUSDT','AUTOBUSD','AMBBTC',
+          'AUTOUSDT','ANKRBNB','ANKRBTC','AVABNB','AVABTC',
+          'AVABUSD','ANKRUSDT','ANTBNB','ANTBTC','ANTBUSD',
+          'ANTUSDT','AVAUSDT','APPCBTC','AVAXBNB','ARBNB',
+          'ARBTC','ARBUSD','AVAXBTC','ARDRBTC','AVAXBUSD',
+          'ARDRUSDT','ARKBTC','AVAXEUR','AVAXTRY','AVAXUSDT',
+          'ASRUSDT','ASTBTC','AXSBNB','ATABNB','ATABTC',
+          'ATABUSD','ATAUSDT','ATMBTC','ATMBUSD','ATMUSDT']
+n = int(input("Enter the number of symbols to crawl: "))
 
+# Select the first n symbols
+selected_symbols = SYMBOL[:n]
+# đến ALTUSDT rồi nha
 # No API key/secret needed for this type of call
 client = Client()
 
@@ -39,12 +41,15 @@ columns = [
 # sothutucophieu = 0
 # while sothutucophieu < 2:
 #     sothutucophieu = sothutucophieu + 1
-with open('tonghop2thang4.csv', 'w') as f:
+filename = f'{n}assetraw.csv'
+
+with open(filename, 'w') as f:
     write = writer(f)
     write.writerow(columns)
     
-    for symbol in SYMBOL:
+    for symbol in selected_symbols :
         klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1DAY, "30 April, 2023", "30 June, 2023")
+        #print(f"The symbol {symbol} has {len(klines)} rows.")  # Print the number of rows
         for kline in klines:
             write.writerow([symbol] + kline)
 
