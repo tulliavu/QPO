2285f009b7db2d5ac8b7f716625bbc8ac97e3bea
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2024-02-29 08:52:38 +0700

    Update compare script

---
+++ compare.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt, csv
+import datetime
+import data, po, model
+import ast, pandas as pd
+from qsee.core.ansatz import Wchain_zxz
+
+num_assets = input("Enter number of assets: ")
+for i in range(100):    
+
+    mu, sigma, asset_list = data.get_mu_sigma('asset.csv', num_assets)
+    qp = po.to_po(mu, sigma, num_assets = num_assets, q = 0.5)
+    resultNumpy = model.numpy(qp)
+    resultQOAO = model.qoao(qp)
+    resultVQE = model.vqe(qp, Wchain_zxz(num_assets, int(num_assets/2)))
+
+    fields = ['assets', 'list selection numpy', 'list selection qoao', 'list selection vqe', 'numpy value', 'qoao value', 'vqe value']
+    row = [[str(asset_list), str(resultNumpy.x.astype(int).tolist()), str(list(resultQOAO.x.astype(int).tolist())), str(list(resultVQE.x.astype(int).tolist())), resultNumpy.fval, resultQOAO.fval, resultVQE.fval]]
+    filename = f"result_{num_assets}.csv"
+    df = pd.read_csv(filename)
+    asset = df.assets.unique()
+    if row[0][0] not in asset:
+        with open(filename, 'a') as csvfile:
+            # Create a csv writer object
+            # Check the value of first cell in row, if it is existing, then skip writing it
+            
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(row)
