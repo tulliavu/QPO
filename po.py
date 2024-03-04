699a59457987c8803ae11f3484eded2509839ca9
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2024-03-04 10:05:29 +0700

    Update csv file

--- po.py
+++ po.py
@@ -12,23 +12,14 @@ def to_po(mu, sigma, **kwargs):
     qp = portfolio.to_quadratic_program()
     return qp
 
-def print_result(result):
-    selection = result.x
-    value = result.fval
-    print("Optimal: selection {}, value {:.4f}".format(selection, value))
-
-    eigenstate = result.min_eigen_solver_result.eigenstate
-    probabilities = (
-        eigenstate.binary_probabilities()
-        if isinstance(eigenstate, QuasiDistribution)
-        else {k: np.abs(v) ** 2 for k, v in eigenstate.to_dict().items()}
-    )
-    print("\n----------------- Full result ---------------------")
-    print("selection\tvalue\t\tprobability")
-    print("---------------------------------------------------")
-    probabilities = sorted(probabilities.items(), key=lambda x: x[1], reverse=True)
-
-    for k, v in probabilities:
-        x = np.array([int(i) for i in list(reversed(k))])
-        value = portfolio.to_quadratic_program().objective.evaluate(x)
-        print("%10s\t%.4f\t\t%.4f" % (x, value, v))
+def hamming(vector1, vector2):
+    """Calculate the Hamming distance between two binary vectors"""
+    if len(vector1) != len(vector2):
+        raise ValueError("Vectors must be of the same length")
+    
+    distance = 0
+    for bit1, bit2 in zip(vector1, vector2):
+        if bit1 != bit2:
+            distance += 1
+    
+    return distance
